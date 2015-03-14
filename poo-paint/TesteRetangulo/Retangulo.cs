00cbdb2d6ec23598dbde63ea2a4375bab569f2d3
Thiago <thiagoap88@gmail.com>
2015-03-13 22:21:58 -0300

    Novo sincronism

---
+++ poo-paint/TesteRetangulo/Retangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteRetangulo
+{
+    public class Retangulo
+    {
+        private int p1;
+        private int p2;
+        private int p3;
+        private int p4;
+
+        public Retangulo(int p1, int p2, int p3, int p4)
+        {
+            // TODO: Complete member initialization
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+            this.p4 = p4;
+        }
+
+
+        internal object Imprime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ poo-paint/TesteRetangulo/TesteRetangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TesteRetangulo;
+
+namespace poo_paint
+{
+    [TestClass]
+    public class TesteRetangulo
+    {
+        [TestMethod]
+        public void TestaImprime()
+        {
+            Retangulo r1 = new Retangulo(10, 20, 50, 60);
+            Assert.AreEqual("retangulo[x:10,y:20,l:50,a:60]", r1.Imprime());
+            Retangulo r2 = new Retangulo(5, 5, 10, 200);
+            Assert.AreEqual("retangulo[x:5,y:5,l:10,a:200]", r2.Imprime());
+        }
+    }
+}
